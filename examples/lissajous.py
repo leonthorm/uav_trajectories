aa339b88264db0391469e60e6c4136e60399e737
simba <l.thormeyer@posteo.de>
2024-12-23 01:34:09 +0100

    add noise to dagger training and validate on new trajectory

---
+++ examples/lissajous.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+numRobots = 1
+
+r = 0.3
+height = 0.7
+w = 2 * np.pi / numRobots
+T = 2 * 2 * np.pi / w
+
+# horizontal circles
+for i in range(numRobots):
+    phase = 2 * np.pi / numRobots * i
+
+    with open(f"timed_waypoints_lissajous{i}.csv", "w") as f:
+        f.write("t,x,y,z,yaw\n")
+        for t in np.linspace(0, T, 1000):
+            f.write("{},{},{},{},{}\n".format(
+                t,
+                r * np.sin(w * t + phase),
+                r * np.sin(2 * w * t + phase),
+                height,
+                0
+            ))
